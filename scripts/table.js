25b6259ea573af09e44356a454a6a5749cf47dcc
Josh Jeppson <joshua.jeppson@usu.edu>
2022-07-14 16:46:50 -0600

    The scripts

---
+++ scripts/table.js
@@ -0,0 +1,45 @@
+
+let FILE_FOUND = 200;
+let FILE_PARSE_READY = 4;
+/**
+ * This only allows us to get data from the same origin url,
+ * meaning we can only get CSV data from https://staminachecker.org/*
+ *
+ * NOTE: the "url" parameter is a SUB-DIRECTORY of the browser origin!
+ * So do not pass in URLs like https://staminachecker.org/dir/file.csv.
+ * Instead, pass in dir/file.csv
+ * */
+function getCSVDataAndAppendToTable(url, tableId, headerInfo, clearTable=true) {
+	let origin = window.location.origin;
+	let table = document.getElementById(tableId);
+	if (clearTable) {
+		table.innerHTML = "";
+	}
+	// Add header
+	table.innerHTML += "<tbody>";
+	table.innerHTML += headerInfo.map(colName => "<th>" + colName + "</th>");
+	table.innerHTML += "</thead>";
+	// Get the full URL of the request
+	let requestURL = origin + "/" + url;
+	console.log("Appending data at url " + requestURL);
+	// Get the data from that URL
+	var tableFile = new XMLHttpRequest();
+	tableFile.open("GET", requestURL, true);
+	table.innerHTML += "<tbody>";
+	tableFile.onreadystatechange = function() {
+		if (tableFile.readyState === FILE_PARSE_READY && tableFile.status === FILE_FOUND) {
+			// Parse the data
+			rawTextData = tableFile.responseText;
+			lines = rawTextData.split('\n');
+			table.innerHTML += (
+				lines.map(line =>
+					"<tr>" + line.split(',').map(td => "<td>" + td + "</td>") + "</tr>"
+				)
+			);
+		}
+		else {
+			console.log("Could not find file at URL: " + requestURL + "!");
+		}
+	}
+	table.innerHTML += "</tbody>";
+}
