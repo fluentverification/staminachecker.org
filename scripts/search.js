1bafaee619e4bf19fa35b34c20a815915798eaa9
Josh Jeppson <joshua.jeppson@usu.edu>
2022-07-21 16:19:32 -0600

    Search is now working

---
+++ scripts/search.js
@@ -0,0 +1,32 @@
+function showSearchBar() {
+	let search = document.getElementById('search');
+	let searchIcon = document.getElementById('search-icon');
+	if (search.style.display == "block") {
+		search.style.display = "none";
+		searchIcon.classList.remove("icon_go-next");
+		searchIcon.classList.add("icon_edit-find");
+	}
+	else {
+		search.style.display = "block";
+		searchIcon.classList.add("icon_go-next");
+		searchIcon.classList.remove("icon_edit-find");
+	}
+}
+function search() {
+	let query = document.getElementById('search').value;
+	console.log("Searching for " + query);
+	document.location.assign("search.html?q=" + query);
+}
+
+
+/*
+ * Taken from https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ * */
+function getParameterByName(name, url = window.location.href) {
+	name = name.replace(/[\[\]]/g, '\\$&');
+	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+	results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
