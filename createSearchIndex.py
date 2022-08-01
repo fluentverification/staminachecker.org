869b1ae6d7a0ea6f2fabe96fd2c1aa72f4bce902
Josh Jeppson <joshua.jeppson@usu.edu>
2022-08-01 10:29:04 -0600

    A couple updates

--- createSearchIndex.py
+++ createSearchIndex.py
@@ -12,6 +12,8 @@ Searches files and creates a search index
 
 # Edit this to be the base URL of your website
 baseURL = "https://staminachecker.org"
+json = False
+recurse = False
 
 def getTitle(openedFile):
 	openedFile.seek(0, 0)
@@ -30,12 +32,20 @@ def getAllContents(openedFile):
 
 def searchFile(filename, out=sys.stdout):
 	f = open(filename, 'r')
-	print(", {", file=out)
-	print(f"title:\"{getTitle(f)}\"", file=out)
-	print(f"date:\"{time.ctime(os.path.getmtime(filename))}\"", file=out)
-	print(f"url:\"{baseURL}/{filename}\"", file=out)
-	print(f"contents:\"{getAllContents(f)}\"", file=out)
-	print("}", file=out)
+	qu = ''
+	if json:
+		qu = '\"'
+		print(", {", file=out)
+	else:
+		print(", [", file=out)
+	print(f"{qu}title{qu}:\"{getTitle(f)}\"", file=out)
+	print(f"{qu}date{qu}:\"{time.ctime(os.path.getmtime(filename))}\"", file=out)
+	print(f"{qu}url{qu}:\"{baseURL}/{filename}\"", file=out)
+	print(f"{qu}contents{qu}:\"{getAllContents(f)}\"", file=out)
+	if json:
+		print("}", file=out)
+	else:
+		print("]", file=out)
 	f.close()
 
 def searchAllFiles(cwd, out=sys.stdout):
@@ -44,4 +54,14 @@ def searchAllFiles(cwd, out=sys.stdout):
 			searchFile(fileName, out)
 
 if __name__=='__main__':
-	searchAllFiles(os.getcwd())
+	if "--json" in sys.argv or "-j" in sys.argv:
+		json = True
+	if "-r" in sys.argv or "--recurse" in sys.argv:
+		recurse = True
+	if not ("-h" in sys.argv or "--help" in sys.argv):
+		searchAllFiles(os.getcwd())
+	else:
+		print("Options:")
+		print("\t--recurse/-r:   Recurses subdirectories")
+		print("\t--help/-h:      Shows this message")
+		print("\t--json/-j:      Exports in JSON")
