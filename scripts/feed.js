155f68a826761ef14d4b58c5debb2d41bb21c930
Josh Jeppson <joshua.jeppson@usu.edu>
2022-09-08 11:14:36 -0600

    Updated script for feed 4

--- scripts/feed.js
+++ scripts/feed.js
@@ -6,16 +6,3 @@ async function getFeedJSON() {
 	let pages = await grabFeed().then(function(result) { return result; });
 	return await pages;
 }
-
-function populateRSSFeed() {
-	const feedJson = (await getFeedJSON());
-	let feedbox = document.getElementById('feedbox');
-	feedbox.innerHTML = "";
-	feedJson.feed.forEach(function(feedItem) {
-		feedbox.innerHTML += "<a href='#'>" + 
-			"<h3>" + feedItem.title + "</h3>" +
-			"<h4>" + feedItem.date + "</h4>" +
-			feedItem.description
-		+ "</a>";
-	});
-}
