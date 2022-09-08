57b776201385b66453d3d60dde72c14bd4bc077d
Josh Jeppson <joshua.jeppson@usu.edu>
2022-09-08 11:02:23 -0600

    Added feed. This will (hopefully) work

---
+++ scripts/feed.js
@@ -0,0 +1,20 @@
+function grabFeed() {
+	return fetch("./feed/feed.json").then(response => response.json());
+}
+
+async function getFeedJSON() {
+	let pages = await grabFeed().then(function(result) { return result; });
+	return await pages;
+}
+
+function populateRSSFeed() {
+	const feedJson = (await getFeedJSON()).feed;
+	let feedbox = document.getElementById('feedbox');
+	feedJson.forEach(function(feedItem) {
+		feedbox.innerHTML += "<a href='#'>" + 
+			"<h3>" + feedItem.title + "</h3>" +
+			"<h4>" + feedItem.date + "</h4>" +
+			feedItem.description
+		+ "</a>";
+	});
+}
