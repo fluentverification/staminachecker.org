0238869e24f464e5795e2e9d7ab0c86c360dae7e
Josh Jeppson <joshua.jeppson@usu.edu>
2024-08-07 12:29:03 -0600

    more frontend styling

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -4,6 +4,8 @@ function closeCollapsible(id, headerId) {
 	let header = document.getElementById(headerId);
 	let headerIcon = document.getElementById(headerId + "-icon");
 	collapsible.style.display = "none";
+	collapsible.style.overflowY = "hidden";
+	collapsible.style.height = "0px";
 	var txtValue = headerIcon.innerHTML;
 	headerIcon.innerHTML = "&#9776;" + txtValue.slice(1, txtValue.length);
 	header.onclick = () => openCollapsible(id, headerId);
@@ -14,6 +16,8 @@ function openCollapsible(id, headerId) {
 	let header = document.getElementById(headerId);
 	let headerIcon = document.getElementById(headerId + "-icon");
 	collapsible.style.display = "block";
+	collapsible.style.overflowY = "visible";
+	collapsible.style.height = "auto";
 	var txtValue = headerIcon.innerHTML;
 	headerIcon.innerHTML = "&times;" + txtValue.slice(1, txtValue.length);
 	header.onclick = () => closeCollapsible(id, headerId);
